e3ab0a5bfa81cfd2a079b1505f16a843220350d2
Ferruccio Balestreri <hi@ferrucc.io>
2020-10-21 17:01:36 +0200

    Update theme screenshot

--- frontend/lib/media.js
+++ frontend/lib/media.js
@@ -1,8 +1,10 @@
 import { getStrapiURL } from "./api";
 
 export function getStrapiMedia(media) {
-  const imageUrl = media.url.startsWith("/")
+  if(media){
+    const imageUrl = media.url.startsWith("/")
     ? getStrapiURL(media.url)
     : media.url;
-  return imageUrl;
+    return imageUrl;
+  }
 }
