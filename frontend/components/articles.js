7084ff0aa1b18a16e5fae5f5edcfc4ed961459e9
Ferruccio Balestreri <hi@ferrucc.io>
2020-11-05 10:04:24 +0100

    Reverse changelog order

--- frontend/components/articles.js
+++ frontend/components/articles.js
@@ -3,12 +3,11 @@ import Card from "./card";
 import { Flex } from "@chakra-ui/core";
 
 const Articles = ({ articles }) => {
-  console.log(articles.reverse())
   return (
     <div>
       <Flex justify='center'>
         <Flex maxWidth={'1200px'} direction='column'>
-          {articles.map(article=> <Card
+          {[...articles].reverse().map(article=> <Card
             article={article}
             key={article.slug}
             />
