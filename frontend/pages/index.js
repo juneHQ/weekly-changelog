e3659409c7bb41348188ade0b6ea6f28d57ecede
Ferruccio Balestreri <hi@ferrucc.io>
2021-01-29 02:13:22 +0100

    Polish changelog page and add CTAs

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,7 +3,7 @@ import ArticleCards from "../components/ArticleCards";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import { fetchAPI } from "../lib/api";
-import { Divider, Flex, Stack, Text } from '@chakra-ui/react';
+import { Button, Divider, Flex, Stack, Text } from '@chakra-ui/react';
 
 const Home = ({ articles, categories, homepage }) => {
   return (
@@ -14,6 +14,10 @@ const Home = ({ articles, categories, homepage }) => {
           <Stack>
             <Text textAlign='center' color='black' fontSize='5xl' fontWeight='bold'>{homepage.hero.title}</Text>
             <Text textAlign='center' color='gray.600'>{homepage.hero.subtitle}</Text>
+            <Flex direction={['column', 'row-reverse']} justify='space-between' pt={4}>
+              <Button as='a' href='https://june.so' variant='outline' size='sm' colorScheme='gray' textAlign='center' m={1}>Sign up for early access</Button>
+              <Button as='a' href='https://twitter.com/JuneDotSo' variant='ghost' size='sm' colorScheme='twitter' textAlign='center' m={1}>Follow us on twitter</Button>
+            </Flex>
           </Stack>
         </Flex>
         <Divider mb={16}/>
