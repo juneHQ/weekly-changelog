ddbd1cde79d9cd2bd84d44f56ee2763123809927
Rodrigo Weilg <contact@rodrigoweilg.com>
2021-03-14 22:58:25 +0100

    font

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,7 +4,7 @@ import "../assets/css/style.css";
 import { createContext } from "react";
 import { getStrapiMedia } from "../lib/media";
 import { fetchAPI } from "../lib/api";
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from "@chakra-ui/react";
 
 // Store Strapi Global object in context
 export const GlobalContext = createContext({});
@@ -30,7 +30,7 @@ const MyApp = ({ Component, pageProps }) => {
 // have getStaticProps. So article, category and home pages still get SSG.
 // Hopefully we can replace this with getStaticProps once this issue is fixed:
 // https://github.com/vercel/next.js/discussions/10949
-MyApp.getInitialProps = async (ctx) => {
+MyApp.getInitialProps = async ctx => {
   // Calls page's `getInitialProps` and fills `appProps.pageProps`
   const appProps = await App.getInitialProps(ctx);
   // Fetch global site settings from Strapi
